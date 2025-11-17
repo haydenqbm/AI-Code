018e7bd781f5fe7b924ab6160cc8b160ddc044ea
anhminhswin <106211785@student.swin.edu.au>
2025-11-17 21:39:35 +0700

    c

--- settings.php
+++ settings.php
@@ -1,22 +1,9 @@
 <?php
-/**
- * Database connection settings
- * This file contains the database connection variables for the Nexora IT Solutions project
- */
+$host = "localhost";
+$user = "root";
+$password = "";       
+$dbname = "nexora_db";
 
-// Database connection parameters
-$host = "localhost";        // Database host (XAMPP default)
-$user = "root";            // Database username (XAMPP default)  
-$password = "";            // Database password (XAMPP default is empty, but might be 'root' or set by user)
-$dbname = "nexora_db";     // Database name for the project
-
-// Create connection string for MySQLi
-$servername = $host;
-$username = $user;
-$db_password = $password;
-$database = $dbname;
-
-// Alternative PDO connection variables if needed
+// Optional PDO DSN
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-
-?>
+?>c
